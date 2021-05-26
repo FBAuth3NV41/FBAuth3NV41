fdd58621f8817138fa719a91233a5fc632024df9
FBAuth3NV41 <coollopez2@gmail.com>
2021-05-26 11:23:51 -0500

    Configuración de Autenticación

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBb6R1Ay5YPYQvgUwJaCDwnw1GP37w8kMk",
+  authDomain: "fbauth3nv41-50edd.firebaseapp.com",
+  projectId: "fbauth3nv41-50edd",
+  storageBucket: "fbauth3nv41-50edd.appspot.com",
+  messagingSenderId: "487873857640",
+  appId: "1:487873857640:web:f78ea9778b3d59168ad3dd",
+  measurementId: "G-2461TG503G"
+  });
